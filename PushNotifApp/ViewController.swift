35388455b5fa47410f89c78195baee322c3ea4b9
Carolyn Blumberg <carolyn.blumberg@tastytrade.com>
2019-07-19 14:08:00 -0500

    sign up

--- PushNotifApp/ViewController.swift
+++ PushNotifApp/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var emailBox: UITextField!
+    @IBOutlet weak var nameBox: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClick(_ sender: Any) {
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        let parameters = ["name": nameBox.text!, "email": emailBox.text!] as [String : Any]
+        //create the url with URL
+        let url = URL(string: "http://localhost:3000/users")! //change the url
+        //create the session object
+        let session = URLSession.shared
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            do {
+                if (try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]) != nil {
+                    print("Successful post request")
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+    }
 }
 

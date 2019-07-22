320654b744f5d79a913c85f40b35b4407e4b4ab0
Sarah Briggs <sarah.briggs@duke.edu>
2019-07-22 14:49:15 -0500

    get trader id from trader name
    to do: implement promises

--- PushNotifApp/SubscribeScreenViewController.swift
+++ PushNotifApp/SubscribeScreenViewController.swift
@@ -24,9 +24,11 @@ class SubscribeScreenViewController: UIViewController {
     
     @IBAction func subscribeToFauzia(_ sender: Any) {
         // post to API
+        let curr_user = UserDefaults.standard.string(forKey: "user_id") ?? ""
+        let curr_trader = getTraderId(traderName: "Fauzia")
         
         //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
-        let parameters = ["trader_id":"1", "user_id": "1"] as [String : Any]
+        let parameters = ["trader_id": curr_trader, "user_id": curr_user] as [String : Any]
         
         //create the url with URL
         let url = URL(string: "http://localhost:3000/subscription")! //change the url
@@ -58,6 +60,44 @@ class SubscribeScreenViewController: UIViewController {
         })
         task.resume()
     }
+    
+    func getTraderId(traderName: String) -> String {
+        var trader_id = ""
+        let parameters = ["name": traderName] as [String : Any]
+        //create the url with URL
+        let url = URL(string: "http://localhost:3000/trader/index")! //change the url
+        //create the session object
+        let session = URLSession.shared
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET" //set http method as POST
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                if !responseJSON.isEmpty {
+                    trader_id = responseJSON["id"] as! String
+                }
+            }
+        })
+        task.resume()
+        return trader_id
+    }
+    
     
 
     /*
